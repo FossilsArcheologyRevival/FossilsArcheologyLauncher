31dfb876593aa28efd1768a730fe615bf32e7371
iLexiconn <me@ilexiconn.net>
2016-06-19 15:02:44 +0200

    Download jar and save version

--- bootstrap/src/main/java/net/ilexiconn/launcher/Bootstrap.java
+++ bootstrap/src/main/java/net/ilexiconn/launcher/Bootstrap.java
@@ -1,5 +1,6 @@
 package net.ilexiconn.launcher;
 
+import com.google.common.base.Charsets;
 import com.google.common.hash.Hashing;
 import com.google.common.io.Files;
 import com.google.gson.Gson;
@@ -9,6 +10,7 @@ import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import net.ilexiconn.launcher.version.Version;
 import net.ilexiconn.launcher.version.VersionAdapter;
+import org.apache.commons.io.FileUtils;
 
 import java.io.*;
 import java.net.URL;
@@ -26,9 +28,13 @@ public class Bootstrap {
     public Version currentVersion;
     public String currentMD5;
 
+    public File bootstrapFile;
+    public File launcherFile;
+
     public Version newerVersion;
     public String newerURL;
-    public String actualMD5;
+
+    public Progressbar progressbar;
 
     public static void main(String[] args) {
         List<String> argumentList = Arrays.asList(args);
@@ -44,43 +50,82 @@ public class Bootstrap {
     public Bootstrap(boolean portable) {
         this.dataDir = portable ? new File(".") : this.getDataFolder();
         this.jsonParser = new JsonParser();
-        this.gson = new GsonBuilder().registerTypeAdapter(Version.class, new VersionAdapter()).create();
+        this.gson = new GsonBuilder().registerTypeAdapter(Version.class, new VersionAdapter()).setPrettyPrinting().create();
 
         File bootstrapDir = new File(this.dataDir, "bootstrap");
+        this.bootstrapFile = new File(bootstrapDir, "bootstrap.json");
+        this.launcherFile = new File(bootstrapDir, "launcher.jar");
+
         if (bootstrapDir.exists()) {
-            try {
-                JsonObject object = this.jsonParser.parse(new FileReader(new File(bootstrapDir, "bootstrap.json"))).getAsJsonObject();
-                this.currentVersion = new Version(object.get("version").getAsString());
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+            if (this.bootstrapFile.exists()) {
+                try {
+                    JsonObject object = this.jsonParser.parse(new FileReader(this.bootstrapFile)).getAsJsonObject();
+                    this.currentVersion = new Version(object.get("version").getAsString());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
-            File launcherFile = new File(bootstrapDir, "launcher.jar");
-            try {
-                this.currentMD5 = Files.hash(launcherFile, Hashing.md5()).toString();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (this.launcherFile.exists()) {
+                try {
+                    this.currentMD5 = Files.hash(this.launcherFile, Hashing.md5()).toString();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            if (!bootstrapDir.mkdirs()) {
+                throw new RuntimeException("Unable to create data directory");
             }
         }
+
+        this.progressbar = new Progressbar();
     }
 
     public void start() throws IOException {
-        Map<Version, JsonObject> map = this.gson.fromJson(new InputStreamReader(new URL(Bootstrap.URL).openStream()), new TypeToken<Map<Version, JsonObject>>(){}.getType());
+        Map<Version, JsonObject> map = this.gson.fromJson(new InputStreamReader(new URL(Bootstrap.URL).openStream()), new TypeToken<Map<Version, JsonObject>>() {
+        }.getType());
         for (Map.Entry<Version, JsonObject> entry : map.entrySet()) {
             int compare = entry.getKey().compareTo(this.currentVersion);
             if (compare > 0) {
-                this.newerVersion = entry.getKey();
-                this.newerURL = entry.getValue().get("url").getAsString();
+                if (this.newerVersion != null) {
+                    if (entry.getKey().compareTo(this.newerVersion) > 0) {
+                        this.newerVersion = entry.getKey();
+                        this.newerURL = entry.getValue().get("url").getAsString();
+                    }
+                } else {
+                    this.newerVersion = entry.getKey();
+                    this.newerURL = entry.getValue().get("url").getAsString();
+                }
             } else if (compare == 0) {
-                this.actualMD5 = entry.getValue().get("md5").getAsString();
+                String actualMD5 = entry.getValue().get("md5").getAsString();
+                if (!this.currentMD5.equals(actualMD5)) {
+                    this.newerVersion = entry.getKey();
+                    this.newerURL = entry.getValue().get("url").getAsString();
+                }
             }
         }
 
         if (this.newerVersion != null) {
             System.out.println("Found newer version " + this.newerVersion);
-            System.out.println(this.newerURL);
-        }
-        if (this.currentMD5 != null && !this.currentMD5.equals(this.actualMD5)) {
-            System.out.println("Invalid jar!");
+            if (this.launcherFile.exists()) {
+                if (!this.launcherFile.delete()) {
+                    throw new RuntimeException("Failed to remove old jar");
+                }
+            }
+            this.progressbar.display(this.newerURL, this.launcherFile, new Progressbar.IProgressbarCallback() {
+                @Override
+                public void call() {
+                    JsonObject object = new JsonObject();
+                    object.addProperty("version", Bootstrap.this.newerVersion.get());
+                    String json = Bootstrap.this.gson.toJson(object);
+                    try {
+                        FileUtils.writeStringToFile(Bootstrap.this.bootstrapFile, json, Charsets.UTF_8);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Complete!");
+                }
+            });
         }
     }
 
---
+++ bootstrap/src/main/java/net/ilexiconn/launcher/ProgressBar.java
@@ -0,0 +1,74 @@
+package net.ilexiconn.launcher;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Progressbar {
+    public void display(final String string, final File target, final IProgressbarCallback callback) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        final JProgressBar progressBar = new JProgressBar();
+        progressBar.setStringPainted(true);
+        progressBar.setSize(300, 70);
+        final JFrame frame = new JFrame();
+        frame.setContentPane(progressBar);
+        frame.setUndecorated(true);
+        frame.setAlwaysOnTop(true);
+        frame.setResizable(false);
+        frame.getContentPane().setBackground(new Color(1.0F, 1.0F, 1.0F, 0.0F));
+        frame.setBackground(new Color(1.0F, 1.0F, 1.0F, 0.0F));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        new Thread() {
+            public void run() {
+                try {
+                    URL url = new URL(string);
+                    HttpURLConnection connection = (HttpURLConnection) (url.openConnection());
+                    long contentLength = connection.getContentLength();
+                    InputStream inputStream = new BufferedInputStream(connection.getInputStream());
+                    OutputStream outputStream = new FileOutputStream(target);
+                    OutputStream bufferedOutputStream = new BufferedOutputStream(outputStream, 1024);
+                    byte[] data = new byte[1024];
+                    long downloaded = 0;
+                    int i;
+                    while ((i = inputStream.read(data, 0, 1024)) >= 0) {
+                        downloaded += i;
+
+                        final int currentProgress = (int) ((((double) downloaded) / ((double) contentLength)) * 100.0D);
+                        SwingUtilities.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressBar.setValue(currentProgress);
+                                progressBar.setString(currentProgress + "%");
+                            }
+                        });
+
+                        bufferedOutputStream.write(data, 0, i);
+                    }
+                    bufferedOutputStream.close();
+                    inputStream.close();
+                    frame.setVisible(false);
+                    frame.dispose();
+                    callback.call();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    public interface IProgressbarCallback {
+        void call();
+    }
+}
